b4cebfdb604345c5b49610594c5f14ea575b0a33
liuyu <liuyu_0102@163.com>
2018-12-01 20:11:14 +0800

    base

--- src/main/java/com/itliuyu/BaseController.java
+++ src/main/java/com/itliuyu/BaseController.java
@@ -3,4 +3,7 @@ package com.itliuyu;
 /*    @Author:liuyu
  *         @键盘敲烂，月薪过万    */
 public class BaseController {
+    public static void main(String[] args) {
+        System.out.println("11111111111111");
+    }
 }
--- src/main/java/com/itliuyu/SecondController.java
+++ src/main/java/com/itliuyu/SecondController.java
@@ -3,9 +3,7 @@ package com.itliuyu;
 /*    @Author:liuyu
  *         @键盘敲烂，月薪过万    */
 public class SecondController {
-
     public static void main(String[] args) {
         System.out.println("测试...." + "测试2");
     }
-
 }
