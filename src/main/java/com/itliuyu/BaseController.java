0d390190a0524edcb0a42940c4c6a8a55217a4f7
liuyu <liuyu_0102@163.com>
2018-12-01 20:13:23 +0800

    2222

--- src/main/java/com/itliuyu/BaseController.java
+++ src/main/java/com/itliuyu/BaseController.java
@@ -3,4 +3,7 @@ package com.itliuyu;
 /*    @Author:liuyu
  *         @键盘敲烂，月薪过万    */
 public class BaseController {
+    public static void main(String[] args) {
+        System.out.println("2222222222222");
+    }
 }
