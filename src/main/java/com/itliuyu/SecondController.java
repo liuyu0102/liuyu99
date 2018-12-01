06bd16ae6a94c9be6ada0598150f0f50ea3a9106
liuyu <liuyu_0102@163.com>
2018-12-01 20:03:50 +0800

    2222

--- src/main/java/com/itliuyu/SecondController.java
+++ src/main/java/com/itliuyu/SecondController.java
@@ -3,4 +3,9 @@ package com.itliuyu;
 /*    @Author:liuyu
  *         @键盘敲烂，月薪过万    */
 public class SecondController {
+
+    public static void main(String[] args) {
+        System.out.println("测试....");
+    }
+
 }
