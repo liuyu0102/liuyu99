c5a8ca7a2ceaad54da81b002825bf9f3bb111950
liuyu <liuyu_0102@163.com>
2018-12-01 20:06:16 +0800

    base

--- src/main/java/com/itliuyu/SecondController.java
+++ src/main/java/com/itliuyu/SecondController.java
@@ -5,7 +5,7 @@ package com.itliuyu;
 public class SecondController {
 
     public static void main(String[] args) {
-        System.out.println("测试....");
+        System.out.println("测试...." + "测试2");
     }
 
 }
