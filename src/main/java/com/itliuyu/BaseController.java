9f70c8688863fb05066fdbe85f21c657a6db3374
liuyu <liuyu_0102@163.com>
2018-12-01 20:19:47 +0800

    2222

--- src/main/java/com/itliuyu/BaseController.java
+++ src/main/java/com/itliuyu/BaseController.java
@@ -5,8 +5,6 @@ package com.itliuyu;
 public class BaseController {
     public static void main(String[] args) {
         System.out.println("2222222222222");
-    }
-    public static void main(String[] args) {
-        System.out.println("11111111111111");
+        System.out.println("1111111111111hjhjhjjkjkjkjk1");
     }
 }
